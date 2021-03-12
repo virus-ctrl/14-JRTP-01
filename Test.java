e5492c2569cee72e52fb0370d74071892b15c160
virendra rajput <vrajput956@gmail.com>
2021-03-12 11:08:05 +0530

    modified Bug-102

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test{
+   
+private int number;
+private float avgsalary;
+private String name;
+private String addrs;
+
+//generate Getter and Setter method
+//alt+shift+s,r
+
+   }
+}
